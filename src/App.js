f7510d29e570b8f670296429ce9d7f9bb6aeaf76
David Harris <dahveed15@gmail.com>
2018-05-23 09:35:56 -0700

    initialize empty game board

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      gameBoard: [
+        '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '',
+        '', '', '', '', '', '', ''
+      ]
+    }
+  }
+  
   render() {
-    return 'test';
+    return (
+      <div className="container">
+        <div className="menu">
+          <h1>Connect Four</h1>
+        </div>
+      </div>
+    );
   }
 }
 
